548ca40a2958c300a34b1cab4c87822f7272a12f
= <NefiteTifall>
2020-10-06 09:06:17 +0200

    Mise à jour pour faire fonctionner le Morpion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Morpion from './componants/morpionIndex.js'
-import Meteo from './componants/meteoIndex.js'
-import Calculatrice from './componants/calculIndex.js'
 import ReactRouter from 'react-router-dom'
 import {
   BrowserRouter as Router,
@@ -16,18 +14,12 @@ function App() {
 		<div>
 			<Switch>
   				<Route path="/morpion">
-					<Morpion />
+					<Morpion/>
 		  		</Route>
-				<Route path="/meteo">
-					<Meteo />
-				</Route>
-				<Route path="/calcul">
-					<Calculatrice />
-				</Route>
 			</Switch>
 		</div>
 	</Router>
-  )	
+  )
 }
 
 export default App;
--- src/componants/game.js
+++ src/componants/game.js
@@ -5,13 +5,13 @@ import './CSS/Morpion.css';
 class Game extends React.Component {
     constructor(props) {
         super(props);
+        console.log(this.props)
         if (this.props.ai) {
             this.state = {
                 history: [{
                     squares: Array(9).fill(null),
                 }],
                 squares: Array(9).fill(null),
-                stepNumber: 0,
                 nb: 0,
                 xIsNext: true,
                 ai: true
@@ -22,7 +22,6 @@ class Game extends React.Component {
                     squares: Array(9).fill(null),
                 }],
                 squares: Array(9).fill(null),
-                stepNumber: 0,
                 nb: 0,
                 xIsNext: true,
                 ai: false
@@ -55,10 +54,9 @@ class Game extends React.Component {
         }
         let status;
         if (winner) status = winner + ' vient de gagner, félicitations !';
-        else if (this.state.nb === 9 || this.state.nb === 10) status = "Match nul, félicitations !";
+        else if (this.state.nb === 9 || this.state.nb === 10) status = "Match Nul";
         else status = 'Prochain joueur : ' + (this.state.xIsNext ? 'X' : 'O');
         if (this.state.ai == true && this.state.xIsNext == false) this.handleClick(bestPosibilitie(current.squares, clearTab))
-        console.log(this.state.nb);
         return (
             <div className="game">
                 <div className="game-info">
@@ -94,7 +92,6 @@ function calculateWinner(carre) {
 function bestPosibilitie(carre, clear) {
     for (var a = 0; a < 9; a++) {
         let testMap = Array.from(carre)
-        console.log(testMap)
         if (testMap[a] == null) testMap[a] = "O"
         if (calculateWinner(testMap) == "O") return a
     }
